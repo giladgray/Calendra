af07167e944bb6f8e037454e122861ade8811d5e
Gilad Gray <giladgray@gmail.com>
2012-09-28 18:33:51 -0700

    calendar time

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development do
+  gem 'sqlite3'
+  gem 'thin'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'bootstrap-sass', '~> 2.1.0.0'
+gem 'jquery-rails'
+gem 'eco'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendra::Application.config.secret_token = '6c891744bc794fc57a3c43fce641d60150af83ac1cc6e6a9bdec887a245c5e5de6d17e9ca9136cab93b974fcde9b16b1fdcb41b172196aa6663a9b6572410421'
